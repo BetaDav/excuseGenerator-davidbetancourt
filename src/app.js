f5bfe77e82041bb33e255de50541012b1ddd1daf
David Betancourt <dmarquez.develop@gmail.com>
2025-05-14 01:44:50 +0000

    Excuse generator done

--- src/app.js
+++ src/app.js
@@ -5,7 +5,27 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'my phone', 'the car'];
+let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+function ramdonElement(array){
+  let ramdonIndex = Math.floor(Math.random() * array.length);
+
+  return array[ramdonIndex];
+}
+
+function generateExcuse(){
+  let subject = ramdonElement(who);
+  let verb = ramdonElement(action);
+  let object = ramdonElement(what);
+  let time = ramdonElement(when);
+
+  return `${subject} ${verb} ${object} ${time}.`;
+}
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let excuse = generateExcuse();
+  document.getElementById('excuse').innerHTML = excuse; 
 };
